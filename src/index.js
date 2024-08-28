148d4de0419bd03abca1a8788d2b2e4576f29774
baibhavthapa <baibhavt@buffalo.edu>
2024-08-28 14:50:56 -0400

    somethings new

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import { PrivyProvider } from '@privy-io/react-auth';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <PrivyProvider
-      appId="process.env.REACT_APP_PRIVY_APP_ID"
+      appId={process.env.REACT_APP_PRIVY_APP_ID}
       config={{
         loginMethods: ['farcaster'],
         appearance: {
